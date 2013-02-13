b40ea8c49854409ee122ae572621f56144d27f47
Angel Palencia <afpccc@gmail.com>
2013-02-13 14:26:10 +0100

    Initial commit

---
+++ Matchismo/Matchismo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Angel on 13/02/13.
+//  Copyright (c) 2013 Angel Palencia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
